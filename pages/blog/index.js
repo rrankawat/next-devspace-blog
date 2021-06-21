eb6c57abd9a993b2a618c64c53e214a6de45d807
rakesh.rankawat <rakesh.rankawat.ext@unit4.com>
2021-06-21 23:02:28 +0530

    Generate Static Paths & Single Post Data

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import Link from 'next/link';
 import matter from 'gray-matter';
 import Layout from '../../components/Layout';
 import Post from '../../components/Post';
