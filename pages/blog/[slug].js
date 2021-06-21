061650e0afba6c2d8fa3f10924abf7a2a206face
rakesh.rankawat <rakesh.rankawat.ext@unit4.com>
2021-06-21 23:22:33 +0530

    Display Single Blog Post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,12 +1,45 @@
 import fs from 'fs';
 import path from 'path';
+import Link from 'next/link';
 import matter from 'gray-matter';
+import marked from 'marked';
+import Layout from '../../components/Layout';
+import CategoryLabel from '../../components/CategoryLabel';
 
 export default function PostPage({ frontmatter, content, slug }) {
-  const { title, category, data, cover_image, author, author_image } =
+  const { title, category, date, cover_image, author, author_image } =
     frontmatter;
 
-  return <div>{title}</div>;
+  return (
+    <Layout title={title}>
+      <Link href="/blog">Back</Link>
+
+      <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+        <div className="flex justify-between items-center mt-4">
+          <h1 className="text-5xl mb-7">{title}</h1>
+          <CategoryLabel>{category}</CategoryLabel>
+        </div>
+
+        <img src={cover_image} alt="" className="w-full rounded" />
+
+        <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+          <div className="flex items-center">
+            <img
+              src={author_image}
+              alt=""
+              className="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block"
+            />
+            <h4>{author}</h4>
+          </div>
+          <div className="mr-4">{date}</div>
+        </div>
+
+        <div className="blog-text mt-2">
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }}></div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
